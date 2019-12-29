4e5d0a86a2b0c934c953b3867be5e0eb82d16430
Arystan1 <arystannyrahmetov@gmail.com>
2019-12-29 15:56:41 +0600

    added linked list #4

--- Tails/LinkedList/src/com/company/LinkedList.java
+++ Tails/LinkedList/src/com/company/LinkedList.java
@@ -2,20 +2,23 @@ package com.company;
 
 public class LinkedList {
 
-    public Node node;
+    private Node Head;
+    private int size = 0;
 
-    public class Node {
+    private static class Node {
 
-        public int element;
-        public Node nextElement;
+        private int element;
+        private Node nextElement;
+        private int index;
 
-        public Node(int el)
+        private Node(LinkedList list, int el)
         {
             Node curr = this;
 
             if(curr.nextElement == null)
             {
                 this.element = el;
+                this.index = list.size;
             }
             else
             {
@@ -25,50 +28,190 @@ public class LinkedList {
                 }
 
                 curr.nextElement.element = el;
+                this.index = list.size;
             }
         }
     }
 
     public LinkedList(int el)
     {
-        this.node = new Node(el);
+        this.Head = new Node(this, el);
+        this.size++;
     }
+
+    public LinkedList(){}
 
     public void add(int el)
     {
-        Node curr = this.node;
+        Node curr = this.Head;
+
+        if(curr == null)
+        {
+            this.Head = new Node(this, el);
+            this.size++;
+
+            return;
+        }
 
         while(curr.nextElement != null)
         {
             curr = curr.nextElement;
         }
 
-        curr.nextElement = new Node(el);
+        curr.nextElement = new Node(this, el);
+        curr.nextElement.index = this.size;
+        this.size++;
+    }
+
+    public void addViaIndex(int index, int el)
+    {
+        Node curr = this.Head;
+        Node firstPart, secondPart;
+
+        //exception index < 0 and index > size
+        if(index > this.size() || index < 0)
+        {
+            System.out.println("Incorrect index!");
+            return;
+        }
+        //exception index = 0
+        if(index == 0)
+        {
+            Node base = new Node(this, el);
+            base.nextElement = curr;
+            base.index = 0;
+
+            //increment next elements
+            Node temp = curr;
+            while(temp != null)
+            {
+                temp.index++;
+                temp = temp.nextElement;
+            }
+
+            this.Head = base;
+            this.size++;
+
+            return;
+        }
+
+        //others
+        //get first and second part
+        while(curr.index < index - 1)
+        {
+            curr = curr.nextElement;
+        }
+
+        secondPart = curr.nextElement;
+
+        //increment next elements
+        Node temp = secondPart;
+        while(temp != null)
+        {
+            temp.index++;
+            temp = temp.nextElement;
+        }
+
+        //collect 2 parts
+        firstPart = curr.nextElement = new Node(this, el);;
+        firstPart.index = index;
+        firstPart.nextElement = secondPart;
+
+        this.size++;
+    }
+
+    public void deleteViaIndex(int index)
+    {
+        Node curr = this.Head;
+        Node firstPart, secondPart;
+        Node temp;
+
+        //exception index < 0 and index > size
+        if(index > this.size() - 1 || index < 0)
+        {
+            System.out.println("Incorrect index!");
+            return;
+        }
+        //exception index = 0
+        if(index == 0)
+        {
+            curr = curr.nextElement;
+
+            //decrement next elements
+            temp = curr;
+            while(temp != null)
+            {
+                temp.index--;
+                temp = temp.nextElement;
+            }
+
+            this.Head = curr;
+            this.size--;
+
+            return;
+        }
+
+        //others
+        //get first and second part
+        while(curr.index < index - 1)
+        {
+            curr = curr.nextElement;
+        }
+
+        temp = curr.nextElement;
+
+        //decrement next elements
+        while(temp != null)
+        {
+            temp.index--;
+            temp = temp.nextElement;
+        }
+
+        //collect 2 parts
+        firstPart = curr;
+        secondPart = curr.nextElement;
+        firstPart.nextElement = secondPart.nextElement;
+
+        this.size--;
+    }
+
+    public int getElement(int index)
+    {
+        Node curr = this.Head;
+
+        while(curr.index < index)
+        {
+            curr = curr.nextElement;
+        }
+
+        return curr.element;
+    }
+
+    public Node getNode(int index)
+    {
+        Node curr = this.Head;
+
+        while(curr.index < index)
+        {
+            curr = curr.nextElement;
+        }
+
+        return curr;
     }
 
     public int size()
     {
-        Node curr = this.node;
-        int size = 1;
-
-        while(curr.nextElement != null)
-        {
-            size++;
-            curr = curr.nextElement;
-        }
-
-        return size;
+        return this.size;
     }
 
     public void print()
     {
-        Node curr = this.node;
+        Node curr = this.Head;
 
         for(int i = 0; i < this.size(); i++)
         {
-            System.out.println(curr + " " + curr.element);
+            System.out.println(curr + " " + curr.index + " " +  curr.element);
             curr = curr.nextElement;
         }
-
     }
 }
--- Tails/LinkedList/src/com/company/Main.java
+++ Tails/LinkedList/src/com/company/Main.java
@@ -4,14 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList newList = new LinkedList(5);
+        LinkedList newList = new LinkedList();
 
-        newList.add(7);
-        newList.add(10);
-        newList.add(45);
-        newList.add(74);
-
+        newList.add(0);
+        newList.add(1);
+        newList.add(2);
+        newList.add(3);
+        newList.addViaIndex(1, 9);
         newList.print();
-
+        System.out.println("-----------");
+        newList.deleteViaIndex(3);
+        newList.print();
+        //System.out.println(newList.getNode(0));
     }
 }
